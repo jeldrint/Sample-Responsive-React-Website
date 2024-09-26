330d9b0e9d827730008ecd62fc25da661d38af20
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2024-09-26 14:11:15 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/codingAssignment/",
+    base: "/",
     plugins: [react()],
+    build: {
+        manifest: true,
+        outDir: 'dist',
+        rollupOptions: {
+            input: ['./src/main.jsx', './index.html']
+        },
+      },  
 })
